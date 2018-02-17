4e206f7f1e29008b70689ab0860d3153951406c5
Moulindra Muchumari <moulindra1729@gmail.com>
2018-02-17 02:29:14 -0500

    remote heroku works

--- pi_client.py
+++ pi_client.py
@@ -3,10 +3,11 @@ import requests
 res_code = -1
 num_tries = 10
 
-url = 'http://localhost:3000/update'
-payload = {'name' : 'test4',
-            'ip': '127.0.0.1',
-            'port': '80',
+#url = 'http://localhost:3000/update'
+url = 'https://raspitracker.herokuapp.com/update'
+payload = {'name' : 'test5',
+            'ip': '127.0.0.3',
+            'port': '7080',
             'status': 'on'}
 while res_code != 200 and num_tries > 0:
     r = requests.get(url, params=payload)
