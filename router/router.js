0760e20a70d59e808761ad0add3de5767e2edde9
Moulindra Muchumari <mouleendhra@nbp-197-156.nbp.ruw.rutgers.edu>
2018-02-16 23:31:36 -0500

    added postgresql db

--- router/router.js
+++ router/router.js
@@ -1,8 +1,20 @@
 var express = require('express')
 var router = express.Router()
+var pg = require('pg');
 
 router.get('/', function(req, res) {
-  res.render('../public/html/index')
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM pi_clients', function(err, result) {
+            done()
+            if(err) {
+                console.log(err)
+                res.send("Error: " + err)
+            } else {
+                response.render('../public/html/index', {results: result.rows})
+            }
+        })
+    })
+  //res.render('../public/html/index')
 })
 
 module.exports = router
